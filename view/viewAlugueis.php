7d5271246175c5eca18614f9ed71f6cbd0e5962e
Denilson21R <45276744+Denilson21R@users.noreply.github.com>
2021-09-14 11:09:27 -0300

    Migração pro Bootstrap 4

--- view/viewAlugueis.php
+++ view/viewAlugueis.php
@@ -11,20 +11,20 @@ $veiculos = $controllerVeiculo->getVeiculos();
         <!-- CSS home-->
         <link href="<?php echo $_SESSION["path"]; ?>view/css/home.css" rel="stylesheet">
         <!-- Bootstrap core CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
         <div class="container">
             
             <header class="navbar" style="justify-content: flex-end;">
                 <div style="margin-right: 20px; font-weight: bolder"><?php echo strtoupper($_SESSION["login_usuario"]); ?></div>
-                <button class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modalLogoff">Sair</button>
+                <button class="btn btn-dark" id="btnLogoff">Sair</button>
             </header>
             <div class="py-5 text-center">
                 <h2>Veículos</h2>
                 
             </div>
-            <button class="btn btn-dark" style="margin-bottom: 5px;" data-bs-toggle="modal" data-bs-target="#modalAddVeiculo" id="btnModalAddVeiculo" >Adicionar Veículo</button>
+            <button class="btn btn-dark" style="margin-bottom: 5px;" id="btnModalAddVeiculo" >Adicionar Veículo</button>
             
             <table class="table table-dark">
                 <thead>
@@ -47,10 +47,10 @@ $veiculos = $controllerVeiculo->getVeiculos();
                             <?php }else if($veiculo["id_dono"] != $_SESSION["id_usuario"] && !$veiculo["alugado"]){ ?>
                             <button type="button" class="btn btn-primary" onclick='alugarVeiculo("<?= $veiculo["id"]; ?>")'>Alugar</button>  
                             <?php }else if($veiculo["id_dono"] == $_SESSION["id_usuario"] && !$veiculo["alugado"]){ ?>                          
-                            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalEditVeiculo" onclick='editVeiculo("<?= $veiculo["id"]; ?>","<?= $veiculo["nome"]; ?>")'>Editar</button>
+                            <button type="button" class="btn btn-success" onclick='editVeiculo("<?= $veiculo["id"]; ?>","<?= $veiculo["nome"]; ?>")'>Editar</button>
                             <button type="button" class="btn btn-danger" onclick='excluirVeiculo("<?= $veiculo["id"]; ?>")'>Excluir</button>
                             <?php }else if($veiculo["id_dono"] == $_SESSION["id_usuario"] && $veiculo["alugado"]){ ?>
-                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalEditVeiculo" onclick='editVeiculo("<?= $veiculo["id"]; ?>","<?= $veiculo["nome"]; ?>")'>Editar</button>
+                                <button type="button" class="btn btn-success" onclick='editVeiculo("<?= $veiculo["id"]; ?>","<?= $veiculo["nome"]; ?>")'>Editar</button>
                             <?php } ?>
                         </td>
                     </tr>
@@ -63,11 +63,13 @@ $veiculos = $controllerVeiculo->getVeiculos();
                     <div class="modal-content">
                     <div class="modal-header  text-center">
                         <h5 class="modal-title" id="exampleModalLabel">Deseja sair?</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                     <div class="modal-footer" style="justify-content: space-evenly;">
                         <button type="button" class="btn btn-primary" id="btnSair">Confirmar</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                     </div>
                     </div>
                 </div>
@@ -80,14 +82,16 @@ $veiculos = $controllerVeiculo->getVeiculos();
                     <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Adicionar Veículo</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                     <div class="modal-body">
                         <label for="Nome">Nome do Veículo</label>
                         <input type="text" class="form-control" id="Nome" required>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                         <button type="button" class="btn btn-primary" id="btnAddVeiculo">Salvar</button>
                     </div>
                     </div>
@@ -101,7 +105,9 @@ $veiculos = $controllerVeiculo->getVeiculos();
                     <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Editar Veículo</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                     <form id="formEditaVeiculo">
                         <input type="hidden" name="id_veiculo_edit" id="id_veiculo_edit">
@@ -110,7 +116,7 @@ $veiculos = $controllerVeiculo->getVeiculos();
                             <input type="text" class="form-control" name="nome_veiculo_edit" id="nome_veiculo_edit" required>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                             <button type="submit" class="btn btn-primary" id="btnSalvaVeiculo">Salvar</button>
                         </div>
                     </form>
@@ -120,14 +126,18 @@ $veiculos = $controllerVeiculo->getVeiculos();
             <!-- End Modal editVeiculo-->
 
             <!-- Modal Concluido -->
-            <div id="myModal" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h3 id="myModalLabel">OK!</h3>
-                </div>
-                <div class="modal-body"><p>Cadastro realizado com sucesso!</p>
-                    <div class="modal-footer">
-                        <button class="btn" data-dismiss="modal" aria-hidden="true">Fechar</button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Concluído com Sucesso!</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-success" onclick="window.location.reload();">Ok</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -136,14 +146,13 @@ $veiculos = $controllerVeiculo->getVeiculos();
     </body>
 </html>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 <script>
 
-var myModal = document.getElementById('myModal')
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+$("#btnLogoff").click(function(){
+    $("#modalLogoff").modal('show');
+});
 
 $("#btnSair").click(function(){
     $.ajax({
@@ -157,9 +166,9 @@ $("#btnSair").click(function(){
         }
     });
 });
-
 $("#btnModalAddVeiculo").click(function(){
     $("#Nome").val("");
+    $("#modalAddVeiculo").modal('show');
 });
 
 $("#btnAddVeiculo").click(function(){
@@ -171,8 +180,8 @@ $("#btnAddVeiculo").click(function(){
             data: {'nomeVeiculo':nome},
             success: function(data) {
                 if(data == ""){
-                    myModal.toggle();
-                    document.location.reload();
+                    $("#modalAddVeiculo").modal('hide');
+                    $("#exampleModal").modal('show');
                 }
             }
         });
@@ -186,15 +195,16 @@ function excluirVeiculo(id){
         data: {'id':id},
         success: function(data) {
             if(data == ""){
-                document.location.reload();
+                $("#exampleModal").modal('show');
             }
         }
     });
 }
 
 function editVeiculo(id, nome){
-    $("#id_veiculo_edit").val(id)
-    $("#nome_veiculo_edit").val(nome)
+    $("#id_veiculo_edit").val(id);
+    $("#nome_veiculo_edit").val(nome);
+    $("#modalEditVeiculo").modal('show');
 }
 
 $("#formEditaVeiculo").submit(function(e){
@@ -205,7 +215,8 @@ $("#formEditaVeiculo").submit(function(e){
         data: $(this).serialize(),
         success: function(data) {
             if(data == ""){
-                document.location.reload();
+                $("#modalEditVeiculo").modal('hide');
+                $("#exampleModal").modal('show');
             }
         }
     });
@@ -218,7 +229,7 @@ function alugarVeiculo(id){
         data: {'id':id},
         success: function(data) {
             if(data == ""){
-                document.location.reload();
+                $("#exampleModal").modal('show');
             }
         }
     });
@@ -231,7 +242,7 @@ function devolverVeiculo(id){
         data: {'id':id},
         success: function(data) {
             if(data == ""){
-                document.location.reload();
+                $("#exampleModal").modal('show');
             }
         }
     });
